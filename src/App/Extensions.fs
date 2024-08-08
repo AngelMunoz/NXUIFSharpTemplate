38c6e6788867c63d2629e59138224c0c8e675070
Angel D. Munoz <scythes.lol@gmail.com>
2024-08-08 17:21:18 -0600

    chode: checkpoint

--- src/App/Extensions.fs
+++ src/App/Extensions.fs
@@ -1,17 +1,40 @@
-namespace App
+[<AutoOpen>]
+module Extensions
 
-open System
+open FSharp.Data.Adaptive
+open Avalonia.Controls
+open Avalonia.Controls.Templates
+open System.Collections
+open Avalonia.Data
+open Navs.Avalonia
 
-[<RequireQualifiedAccess>]
-module Observable =
 
-  let tap
-    (observe: 'Type -> unit)
-    (observable: IObservable<'Type>)
-    : IObservable<'Type> =
-    Observable.map
-      (fun value ->
-        observe value
-        value
-      )
-      observable
+type ItemsRepeater with
+
+  member this.itemTemplate(template: IDataTemplate) =
+    this.ItemTemplate <- template
+    this
+
+  member this.itemsSource(items: IEnumerable) =
+    this.ItemsSource <- items
+    this
+
+  member this.itemsSource(items: aval<_>) =
+    let binding =
+      ItemsRepeater.ItemsSourceProperty
+        .Bind()
+        .WithMode(BindingMode.OneWay)
+        .WithPriority(BindingPriority.LocalValue)
+
+    this[binding] <- AVal.toBinding items
+    this
+
+type Grid with
+
+  member this.columnDefinitions(value) =
+    this.ColumnDefinitions <- ColumnDefinitions(value)
+    this
+
+  member this.rowDefinitions(value) =
+    this.RowDefinitions <- RowDefinitions(value)
+    this
